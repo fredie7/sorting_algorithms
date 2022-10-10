b614d9399d47f4c2e8c4b34c1231a8e4b5b94871
fredie7 <52379604+fredie7@users.noreply.github.com>
2022-10-10 20:59:55 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,48 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - Use bubble sort method to sort an array
- *
- * @array: The array to be sorted
- * @size: The size of the array
+ *insertion_sort_list - sort list using insertion algorithm
+ *@list: list to be sorted
  */
 
-void bubble_sort(int *array, size_t size)
+void insertion_sort_list(listint_t **list)
 {
-	size_t i;
-	size_t j;
-	int temp;
+	listint_t *top, *prev, *next;
 
-	for (i = 0; i < size; i++)
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	top = (*list)->next;
+	while (top)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		if (top->prev && top->prev->n > top->n)
 		{
-			if (array[j] > array[j + 1])
+			while (top->prev)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				prev = top->prev;
+				if (top->prev && top->n < top->prev->n)
+				{
+					if (top->prev->prev)
+						top->prev->prev->next = top;
+					next = top->next;
+					top->prev->next = next;
+					if (next)
+						next->prev = top->prev;
+					top->next = prev;
+					if (prev)
+					{
+						top->prev = prev->prev;
+						prev->prev = top;
+					}
+					else
+						top->prev = prev;
+					if (!top->prev)
+						*list = top;
+					print_list(*list);
+				}
+				else
+					break;
 			}
 		}
+		top = top->next;
 	}
 }
